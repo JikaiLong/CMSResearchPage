c28c9abd84c25808413f5bc65f241e86fc149ffd
Aron Wisuvanathan <wisuvana@IITS-I406-16.utsc-labs.utoronto.ca>
2018-10-10 15:16:12 -0400

    Added Gender study research section

---
+++ my-app/src/components/projects/Gender Study Research/genderstudy.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './genderstudy.css';
+
+class genderstudy extends Component {
+  render() {
+    return (
+      <div className="Genderstudy">
+        <header>
+          <div className = "GenderstudyTitle">
+            GenderStudy
+            </div>
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default genderstudy;
