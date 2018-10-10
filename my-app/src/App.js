e8b928330589dcefdca5ac55cd7bae7d6bb246b2
Jikai Long <longjika@IITS-I406-17.utsc-labs.utoronto.ca>
2018-10-10 14:13:33 -0400

    add components directories

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Codeclash from './components/projects/Code Clash/codeclash.js';
 
 class App extends Component {
   render() {
     return (
       <div className="CMS Research Project">
+        <header>
+          <div className = "Title">
+            CMS Research Projects
+            </div>
+        </header>
+        <li id = 'codeclashsection'>
+          <Codeclash />
+          </li>
       </div>
     );
   }
---
+++ my-app/src/components/projects/Code Clash/codeclash.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './codeclash.css';
+
+class codeclash extends Component {
+  render() {
+    return (
+      <div className="CodeClash">
+        <header>
+          <div className = "CodeclashTitle">
+            Code Clash
+            </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default codeclash;
